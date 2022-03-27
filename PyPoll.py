1cfc021ce4fd7448187e8607650c62e92a5b2c4e
Olivia Snyder <oliviasny2@gmail.com>
2022-03-27 05:11:27 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -17,16 +17,16 @@ file_to_load = os.path.join("Resources","election_results.csv")
 file_to_save = os.path.join("analysis","election_analysis.txt")
 #outfile = open(file_to_save, "w")
 ## Use "with" instead
-with open(file_to_load) as election_data:
+#with open(file_to_load) as election_data:
     #txt_file.write("Arapahoe, ")
     #txt_file.write("Denver, ")
     #txt_file.write("Jefferson")
     #or just put on one line 
     #txt_file.write("Arapahoe\nDenver\nJefferson")
-    file_reader = csv.reader(election_data)
+    #file_reader = csv.reader(election_data)
 #outfile.close()
-    for row in file_reader:
-        print(row)
+    #for row in file_reader:
+        #print(row)
     ## Going to initilize a variable, AKA accumulator
 total_votes = 0
 candidate_options = []
@@ -47,25 +47,32 @@ with open (file_to_load) as election_data:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
-    print(total_votes)        
-    print(candidate_options)
-    print(candidate_votes)
-
+with open(file_to_save, "w") as txt_file:
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end = "")
+    txt_file.write(election_results)
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
         vote_percentage = (float(votes) / float(total_votes)) * 100
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        txt_file.write(candidate_results)
         if (votes>winning_count) and (vote_percentage > winning_percentage):
             winning_count = votes
-            winning_percentage = vote_percentage
             winning_candidate = candidate_name
+            winning_percentage = vote_percentage
     winning_candidate_summary = (
         f"-----------------------\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
         f"-----------------------\n")
-    print (winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
+    #print (winning_candidate_summary)
 
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes

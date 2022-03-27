5fdffe9df837d4b20a315c869ca84eec019b0a79
Olivia Snyder <oliviasny2@gmail.com>
2022-03-27 04:19:40 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -25,10 +25,48 @@ with open(file_to_load) as election_data:
     #txt_file.write("Arapahoe\nDenver\nJefferson")
     file_reader = csv.reader(election_data)
 #outfile.close()
-    #for row in file_reader:
-        #print(row)
+    for row in file_reader:
+        print(row)
+    ## Going to initilize a variable, AKA accumulator
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+    ## Open  the election results and read the file
+with open (file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
     header = next(file_reader)
-    print(header)
+
+    for row in file_reader:
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+    print(total_votes)        
+    print(candidate_options)
+    print(candidate_votes)
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        vote_percentage = (float(votes) / float(total_votes)) * 100
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        if (votes>winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+        f"-----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-----------------------\n")
+    print (winning_candidate_summary)
+
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
